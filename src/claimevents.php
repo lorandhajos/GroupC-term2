99543b4fd24effb3de2ccb989080dfc6652fd039
Marijn Veenstra <marijn.veenstra1@student.nhlstenden.com>
2022-12-07 15:36:28 +0100

    Add comments in claimevents.php

--- src/claimevents.php
+++ src/claimevents.php
@@ -1,18 +1,21 @@
 <?php
-// user_id being the id of the person that is logged in
 
 include_once('config.php');
 
+// check whether the form has been submitted
 if (isset($_POST["submit"])) {
-    try {
-        $sql = "INSERT INTO Claims(user_id, event_id) VALUES (:user_id, :event_id)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bindValue(':user_id', $_SESSION["user_id"]);
-        $stmt->bindValue(':event_id', $_POST["event_id"]);
-        $stmt->execute();
-      } catch (Exception $e) {
-        $error = "Failed to sent data to the database/claim event";
-      }
+  try {
+    // preparing the sql query
+    // put the value of the superglobal $_SESSION["user_id"] into the user_id that will be connected to the event
+    // and then again but for the event_id
+    $sql = "INSERT INTO Claims(user_id, event_id) VALUES (:user_id, :event_id)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindValue(':user_id', $_SESSION["user_id"]);
+    $stmt->bindValue(':event_id', $_POST["event_id"]);
+    $stmt->execute();
+  } catch (Exception $e) {
+    echo $e;
+  }
 }
 
 ?>
