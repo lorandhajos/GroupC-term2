a4bbfe87961dce76ca588c8ddef568238bb3fe86
CadarIustin <iustin.cadar@student.nhlstenden.com>
2022-12-20 15:58:36 +0000

    I changed the code for the site to link better with the rest of the pages
    - i improved the style of the site
    - i connected the page with the navMenu.php
    - i connected the page with the config.php
    - i added the footer
    - i added the header
    - i conected  the links to the other pages of the site

--- src/profile.php
+++ src/profile.php
@@ -1,28 +1,71 @@
+<?php
+  session_start();
+  include_once "config.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
   <?php include "head.php" ?>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Profile Page</title>
-  </head>
-  
-        <body>
-    <main> 
-  <?php include "navMenu.php" ?>
-    </main>
-    <p id="Name"><strong>Name:</strong></p>
-	<p id="Email"><strong>Email:</strong></p>
-	<p id="Speacialty"><strong>Specialty:</strong></p>
-	<p id="Claimed Events"><strong>Claimed Events:</strong></p>
-	<p id="Events"><strong>Events Created:</strong></p>
-	<form action="profile.php" method="post"> </p>
-	    <h1><strong>Change Password</strong></h2>
-		<p> <input type="text" name="pass" placeholder="Current password..."> </p>
-		<p> <input type="text" name="newPass" placeholder="New password..."> </p>
-		<p> <input type="text" name="confirmPass" placeholder="Confirm password..."> </p>
-		<p> <input type="submit" value="Submit">
-		<p id="terms">Copyright © 2022- Gemorskos. All rights reserved<p>
-	</form>
-	</body>
-</html>
+  <link href="styles/home.css" rel="stylesheet">
+  <title>Profile Page</title>
+</head>
+<body>
+  <main>
+    <div class="row m-0">
+      <div class="col-auto p-0">
+        <nav class="sidebar">
+          <?php include "navMenu.php" ?>
+        </nav>
+      </div>
+      <div class="col p-0">
+        <header class="py-4 shadow-sm">
+        </header>
+        <div class="p-4">
+          <div>
+            <?php
+            echo "<p><strong>Name: " . $_SESSION['name'] . "</strong></p>";
+            echo "<p><strong>Email: " . $_SESSION['email'] . "</strong></p>";
+            echo "<p><strong>Specialty: " . $_SESSION['speciality'] . "</strong></p>";
+            /*try {
+              $sql = "SELECT COUNT(file_id) FROM Files WHERE user_id=:session_id";
+              $stmt = $conn->prepare($sql);
+              $stmt->bindValue(':session_id', $_SESSION['user_id']);
+              $stmt->execute();
+            } catch (Exception $e) {
+              $error = "Failed to connect to the database";
+            }
+            $result = $stmt->fetch(PDO::FETCH_OBJ);
+            echo $result["COUNT(file_id)"];
+            */
+            
+            
+            ?> 
+			
+			 </div>
+          <hr class="my-4">
+          <div>
+            <p><strong>Change Password</strong></p>
+            <div class="col-sm-6">
+              <input type="text" name="currentPassword" class="form-control" id="CurrentPassword" placeholder="Current Password..." value="">
+            </div>
+            <p></p>
+            <div class="col-sm-6">
+              <input type="text" name="newPassword" class="form-control" id="NewPassword" placeholder="New Password..." value="">
+            </div>
+            <p></p>
+            <div class="col-sm-6">
+              <input type="text" name="confirmPassword" class="form-control" id="ConfirmPassword" placeholder="Confirm Password..." value="">
+            </div>
+            <p></p>
+            <button type="button" class="btn btn-primary" type="submit">Submit</button>
+          </div>
+        </div>
+        
+        <footer class="py-3 mt-5 d-flex justify-content-end shadow border-top navbar p-0">
+          <p class="mb-0 me-4">Copyright 2022 - Gemorskos. All rights reserved</p>
+        </footer>
+      </div>
+    </div>
+  </main>
+</body>
+</html> 
