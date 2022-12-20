8136879b08bedc8f958909239abfedc077cc2716
CadarIustin <iustin.cadar@student.nhlstenden.com>
2022-12-20 14:53:57 +0000

    Started implementing the frontend for the page
    -i added the paragraphs
    -i created the button for submiting the new password

--- src/profile.php
+++ src/profile.php
@@ -1,10 +1,25 @@
-<!DOCTYPE>
-<html>
-<head>
-
-</head>
-
-<body>
-
-</body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <?php include "head.php" ?>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Profile Page</title>
+  </head>
+  
+        <body>
+    <main> 
+  <?php "navMenu.php" ?>
+    </main>
+    <p id="Name"><strong>Name:</strong></p>
+	<p id="Email"><strong>Email:</strong></p>
+	<p id="Speacialty"><strong>Specialty:</strong></p>
+	<p id="Claimed Events"><strong>Claimed Events:</strong></p>
+	<p id="Events"><strong>Events Created:</strong></p>
+	<form action="profile.php" method="post"> </p>
+		<p> <input type="text" name="pass" placeholder="Current password"> </p>
+		<p> <input type="text" name="newPass" placeholder="New password"> </p>
+		<p> <input type="text" name="confirmPass" placeholder="Confirm password"> </p>
+		<p> <input type="submit" value="Change Password">
+	</form>
 </html>
