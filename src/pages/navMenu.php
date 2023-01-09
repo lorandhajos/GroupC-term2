8faad80707f8f0b02b728897a5e0ff4c88844e79
Marijn Veenstra <marijn.veenstra1@student.nhlstenden.com>
2023-01-09 09:56:48 +0100

    corrected the indentation in navmenu.php

--- src/pages/navMenu.php
+++ src/pages/navMenu.php
@@ -6,28 +6,26 @@
 
 </div>
   <div class="d-flex flex-column flex-shrink-0 text-bg-dark h-100 justify-content-between ">
-  <!-- <hr> -->
-  <div>
-  <div class="profile ">
-    <a href="#" class="d-flex align-items-center text-white text-decoration-none p-3">
-      <?php echo "<strong>" . $_SESSION['name'] . "</strong>";?>
-    </a>
+    <div>
+      <div class="profile">
+        <a href="#" class="d-flex align-items-center text-white text-decoration-none p-3">
+          <?php echo "<strong>" . $_SESSION['name'] . "</strong>";?>
+        </a>
+      </div>
+    <div class="brandlink"></div>
+    <ul class="nav nav-pills flex-column">
+      <li class="nav-item">
+        <a href="../home.php" class="nav-link text-white" aria-current="page">
+          Events
+        </a>
+      </li>
+      <li>
+        <a href="../pages/newEvent.php" class="nav-link text-white">
+          Create event
+        </a>
+      </li>
+    </ul>
   </div>
-  <!-- <hr> -->
-  <div class="brandlink"></div>
-  <ul class="nav nav-pills flex-column">
-    <li class="nav-item">
-      <a href="../home.php" class="nav-link text-white" aria-current="page">
-        Events
-      </a>
-    </li>
-    <li>
-      <a href="../pages/newEvent.php" class="nav-link text-white">
-        Create event
-      </a>
-    </li>
-  </ul>
-</div>
   <div class="d-flex align-items-end">
     <a href="../logout.php" class="nav-link text-white signOutText pb-5 mb-5 p-3">
       Sign Out
@@ -35,6 +33,3 @@
     </a>
   </div>
 </div>
-<!-- 
-HELP IT WORKS IN CHROME, NOT IN FIREFOX. ALSO THE NEW EVENT PAGE IS ALSO STILL FUCKED !!!!!!!!!!!!!!!!!!!!!
- -->
