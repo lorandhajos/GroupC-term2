28671517f635acc8d6e3a99e296d8187fd1d898a
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2023-01-10 10:21:57 +0100

    Changed h1s to h2s in home page

--- src/home.php
+++ src/home.php
@@ -29,7 +29,7 @@
         <div>
           <header class="headerheight shadow-sm"></header>
           <div class="mx-5">
-            <h1 class="mt-4 mb-4">Your Events</h1>
+            <h2 class="mt-4 mb-4">Your Events</h2>
             <div class="accordion" id="accordionExample1">
               <?php
                 $stmt = $conn->prepare("SELECT * FROM Claims INNER JOIN Users ON Claims.user_id = Users.user_id INNER JOIN Events ON Claims.event_id = Events.event_id WHERE Users.user_id = :id");
@@ -69,7 +69,7 @@
                 }
               ?>
             </div>
-            <h1 class="mt-4 mb-4">Unclaimed Events</h1>
+            <h2 class="mt-4 mb-4">Unclaimed Events</h2>
             <div class="accordion" id="accordionExample2">
               <?php
                 $stmt = $conn->prepare("SELECT * FROM Events WHERE NOT EXISTS (SELECT * FROM Claims WHERE Events.event_id = Claims.event_id)");
@@ -114,7 +114,7 @@
                 }
               ?>
             </div> 
-            <h1 class="mt-4 mb-4">Claimed Events</h1>
+            <h2 class="mt-4 mb-4">Claimed Events</h2>
             <div class="accordion" id="accordionExample2">
               <?php
                 $stmt = $conn->prepare("SELECT * FROM Events WHERE EXISTS (SELECT * FROM Claims WHERE Events.event_id = Claims.event_id)");
