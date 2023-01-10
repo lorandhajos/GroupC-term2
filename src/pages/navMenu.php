30ac5544f2f201cf8acb317bef7231d6dbb36ae4
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2023-01-10 10:12:00 +0100

    improved navMenu indentation

--- src/pages/navMenu.php
+++ src/pages/navMenu.php
@@ -1,17 +1,15 @@
 <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark brandlink">
-  
-    <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      <span class="fs-4"><img src="../images/bootstrap-logo.svg" alt="G" width="44" height="44">emorskos</span>
-    </a>
-
+  <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+    <span class="fs-4"><img src="../images/bootstrap-logo.svg" alt="G" width="44" height="44">emorskos</span>
+  </a>
 </div>
 <div class="d-flex flex-column flex-shrink-0 text-bg-dark h-100 justify-content-between ">
   <div>
-      <div class="profile">
-        <a href="#" class="d-flex align-items-center text-white text-decoration-none p-3">
-          <?php echo "<strong>" . $_SESSION['name'] . "</strong>";?>
-        </a>
-      </div>
+    <div class="profile">
+      <a href="#" class="d-flex align-items-center text-white text-decoration-none p-3">
+        <?php echo "<strong>" . $_SESSION['name'] . "</strong>";?>
+      </a>
+    </div>
     <div class="brandlink"></div>
     <ul class="nav nav-pills flex-column">
       <li class="nav-item">
