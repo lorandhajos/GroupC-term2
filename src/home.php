fdc041ba7c3ac0a6640f1c1cd7b603bc24667d14
Marijn Veenstra <marijn.veenstra1@student.nhlstenden.com>
2023-01-10 15:00:00 +0100

    connected the upload form to each event

--- src/home.php
+++ src/home.php
@@ -37,6 +37,7 @@
 
                 $yourEventId = 0;
                 while ($results = $stmt->fetch()) {
+                  
                   $eventName = $results["name"];
                   $eventId = $results["event_id"];
                   $description = $results["description"];
@@ -63,58 +64,68 @@
                             Select file to upload:
                             <input type='file' name='fileToUpload' id='fileToUpload' multiple>
                             <input type='submit' value='Upload File' name='submit' class='btn btn-primary'>
+                            <input type='hidden' name='eventId' value='$eventId'>
                           </form>
                         </div>
                       </div>
                     </div>
                   </div>";
                   $yourEventId ++;
-                }
-                if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                  $target_dir = 'uploads/' . $eventId . '/';
-                  $target_file = $target_dir . basename($_FILES['fileToUpload']['name']);
-                  $uploadOk = 1;
-                  $FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-                  //var_dump($_POST);
-                  //check if the directory already exists. 
-                  if (is_dir($target_dir) == false) {
-                      // make dir with the name $target_dir
-                      mkdir($target_dir);
-                  }
-                           
-                  // Check if file already exists
-                  if (file_exists($target_file)) {
-                    echo "Sorry, file already exists. ";
-                    $uploadOk = 0;
-                  }
-              
-                  // Check file size
-                  if ($_FILES["fileToUpload"]["size"] > 500000) {
-                    echo "Sorry, your file is too large. ";
-                    $uploadOk = 0;
-                  }
-              
-                  // Allow certain file formats
-                  if($FileType != "jpg" && $FileType != "png" && $FileType != "jpeg"
-                  && $FileType != "gif" && $FileType != "docx" && $FileType != "txt" 
-                  && $FileType != "odt" && $FileType != "dotx" && $FileType != "doc" 
-                  && $FileType != "docm") {
-                    echo "Sorry, your files are not allowed. ";
-                    $uploadOk = 0;
-                  }
-              
-                  // Check if $uploadOk is set to 0 by an error
-                  if ($uploadOk == 0) {
-                    echo "Sorry, your file was not uploaded. ";
-                  // if everything is ok, try to upload file
-                  } else {
-                    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                      echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-                    } else {
-                      echo "Sorry, there was an error uploading your file. ";
+                  // var_dump($_POST);
+                } 
+                  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                    $eventId = filter_input(INPUT_POST, 'eventId', FILTER_VALIDATE_INT);
+                    $target_dir = 'uploads/' . $eventId . '/';
+                    $target_file = $target_dir . basename($_FILES['fileToUpload']['name']);
+                    $uploadOk = 1;
+                    $FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+                    //var_dump($_POST);
+                    //check if the directory already exists. 
+                    
+                    if (is_dir($target_dir) == false) {
+                        // make dir with the name $target_dir
+                        mkdir($target_dir);
                     }
+                            
+                    // Check if file already exists
+                    if (file_exists($target_file)) {
+                      echo "Sorry, file already exists. ";
+                      $uploadOk = 0;
+                    }
+                
+                    //php.ini still needs to be changed to allow files more than 2mb!!!
+
+                    // // Check file size
+                    // if ($_FILES["fileToUpload"]["size"] > 500000) {
+                    //   echo "Sorry, your file is too large. ";
+                    //   $uploadOk = 0;
+                    // }
+                
+                    // Allow certain file formats
+                    if($FileType != "jpg" && $FileType != "png" && $FileType != "jpeg"
+                    && $FileType != "gif" && $FileType != "docx" && $FileType != "txt" 
+                    && $FileType != "odt" && $FileType != "dotx" && $FileType != "doc" 
+                    && $FileType != "docm") {
+                      echo "Sorry, your files are not allowed. ";
+                      $uploadOk = 0;
+                    }
+                  
+                    // Check if $uploadOk is set to 0 by an error
+                    if ($uploadOk == 0) {
+                      echo "Sorry, your file was not uploaded. ";
+                    // if everything is ok, try to upload file
+                    } else {
+                      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+                      } else {
+                        if ( !empty($_FILES)) {
+                        echo "Sorry,x there was an error uploading your file. ";}
+                      }
+                    }
+                    // unset($target_dir, $target_file, $uploadOk, $FileType, $eventId);
+                    var_dump($eventId);
                   }
-                }
+                
               ?>
             </div>
             <h2 class="mt-4 mb-4">Unclaimed Events</h2>
