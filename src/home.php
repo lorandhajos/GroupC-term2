757602f80ecd9bf6122737dd413cd7d934ba7a87
Marijn Veenstra <marijn.veenstra1@student.nhlstenden.com>
2023-01-10 13:00:21 +0100

    able to upload files and perform checks on said files

--- src/home.php
+++ src/home.php
@@ -61,7 +61,7 @@
                           </div>
                           <form action='home.php' method='post' enctype='multipart/form-data'>
                             Select file to upload:
-                            <input type='file' name='fileToUpload' id='fileToUpload'>
+                            <input type='file' name='fileToUpload' id='fileToUpload' multiple>
                             <input type='submit' value='Upload File' name='submit' class='btn btn-primary'>
                           </form>
                         </div>
@@ -69,6 +69,51 @@
                     </div>
                   </div>";
                   $yourEventId ++;
+                }
+                if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                  $target_dir = 'uploads/' . $eventId . '/';
+                  $target_file = $target_dir . basename($_FILES['fileToUpload']['name']);
+                  $uploadOk = 1;
+                  $FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+                  //var_dump($_POST);
+                  //check if the directory already exists. 
+                  if (is_dir($target_dir) == false) {
+                      // make dir with the name $target_dir
+                      mkdir($target_dir);
+                  }
+                           
+                  // Check if file already exists
+                  if (file_exists($target_file)) {
+                    echo "Sorry, file already exists. ";
+                    $uploadOk = 0;
+                  }
+              
+                  // Check file size
+                  if ($_FILES["fileToUpload"]["size"] > 500000) {
+                    echo "Sorry, your file is too large. ";
+                    $uploadOk = 0;
+                  }
+              
+                  // Allow certain file formats
+                  if($FileType != "jpg" && $FileType != "png" && $FileType != "jpeg"
+                  && $FileType != "gif" && $FileType != "docx" && $FileType != "txt" 
+                  && $FileType != "odt" && $FileType != "dotx" && $FileType != "doc" 
+                  && $FileType != "docm") {
+                    echo "Sorry, your files are not allowed. ";
+                    $uploadOk = 0;
+                  }
+              
+                  // Check if $uploadOk is set to 0 by an error
+                  if ($uploadOk == 0) {
+                    echo "Sorry, your file was not uploaded. ";
+                  // if everything is ok, try to upload file
+                  } else {
+                    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                      echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+                    } else {
+                      echo "Sorry, there was an error uploading your file. ";
+                    }
+                  }
                 }
               ?>
             </div>
