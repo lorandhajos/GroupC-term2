cf401a43a70ddef7887a38b4953c3f3874bf50f1
Marijn Veenstra <marijn.veenstra1@student.nhlstenden.com>
2023-01-10 12:49:42 +0100

    added form to upload files

--- src/home.php
+++ src/home.php
@@ -59,7 +59,11 @@
                           <span class='input-group-text' id='addon-wrapping'><i class='fa-solid fa-calendar-days'></i></span>
                           <p class='form-control mb-0'>$eventDate</p>
                           </div>
-                          <a href='home.php' class='btn btn-primary' role='button'>Upload Files</a>
+                          <form action='home.php' method='post' enctype='multipart/form-data'>
+                            Select file to upload:
+                            <input type='file' name='fileToUpload' id='fileToUpload'>
+                            <input type='submit' value='Upload File' name='submit' class='btn btn-primary'>
+                          </form>
                         </div>
                       </div>
                     </div>
