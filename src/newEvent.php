ef157a5a9d8555e16511c1f3ebbe19e3e3579c2d
Andrii Chumakov <andrii.chumakov@student.nhlstenden.com>
2023-01-11 08:40:43 +0100

    added auto-claim feature and an "event created successfully" message

--- src/newEvent.php
+++ src/newEvent.php
@@ -3,8 +3,30 @@
   // check if the user is logged in
   if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: ../index.php");
+  }
+  // get env variables from config.php and 
+  // setup a database connection using a PDO
+  include("pages/config.php");
+  try {
+    $connection = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+  }
+  catch (PDOexception $e) {
+    echo $e. "<br>";
+  }
+  //get the user speciality for future use
+  try {
+    $sql = "SELECT speciality FROM Users WHERE user_id=:user_id;";
+    $stmt = $connection->prepare($sql);
+    $stmt->bindValue(":user_id", $_SESSION["user_id"]);
+    $stmt->execute();
+  }
+  catch (PDOexception $e) {
+    echo $e. "<br>";
+  }
+  $userSpeciality = $stmt->fetch(PDO::FETCH_ASSOC)["speciality"];
 
-  if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  // form validation
+  if (isset($_POST["submit"])) {
     $errs = array();
     if (empty($_POST["eventTitle"])) {
       $errs[] = "please enter a title for the event";
@@ -37,13 +59,9 @@
       echo "<br>";
     }
     $creationDate = date("y-m-d");
-    include("config.php");
-    try {
-      $connection = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-    }
-    catch (PDOexception $e) {
-      echo $e. "<br>";
-    }
+    /*
+    if all the relevant information has been sent, the page should go to a database and create it
+     */
     if (count($errs) == 0) {
       // generate an event ID by using the maximum ID the database and adding 1
       // could be replaced with something more sophisticated
@@ -62,6 +80,7 @@
       else {
         $eventID = 1;
       }
+      // add the event into Events table
       try {
         $sql = "INSERT INTO Events (event_id, name, description, event_date, creation_date) VALUES (:event_id, :event_title, :event_desc, :ev_date, :ev_created)";
         $stmt=$connection->prepare($sql);
@@ -74,6 +93,19 @@
       }
         catch (PDOexception $e) {
         echo $e. "<br>";
+      }
+      // claim the event as the user if said user has photographer or journalist speciality
+      if ($userSpeciality == "photographer" || $userSpeciality == "journalist") {
+        try {
+          $sql = "INSERT INTO Claims(user_id, event_id) VALUES (:user_id, :event_id)";
+          $stmt = $connection->prepare($sql);
+          $stmt->bindValue(':user_id', $_SESSION["user_id"]);
+          $stmt->bindValue(':event_id', $eventID);
+          $stmt->execute();
+        } 
+        catch (Exception $e) {
+          echo $e;
+        }
       }
     }
   }
@@ -101,28 +133,28 @@
               <div class="row g-3">
                 <div class="col-sm-6">
                   <label for="eventTitle" class="form-label">Event Title</label>
-                  <input type="text" class="form-control" id="eventTitle" placeholder="" value="" required>
+                  <input type="text" class="form-control" name="eventTitle" placeholder="" value="" required>
                   <div class="invalid-feedback">
                     Event title is required.
                   </div>
                 </div>
                 <div class="col-sm-6">
                   <label for="eventDate" class="form-label">Date</label>
-                  <input type="date" class="form-control" id="eventDate" placeholder="" value="" required>
+                  <input type="date" class="form-control" name="eventDate" placeholder="" value="" required>
                   <div class="invalid-feedback">
                     Date is required.
                   </div>
                 </div>
                 <div class="col-12">
                   <label for="eventDesc" class="form-label">Details</label>
-                  <textarea class="form-control" id="eventDesc" rows="5"> </textarea>
+                  <textarea class="form-control" name="eventDesc" rows="5"> </textarea>
                   <div class="invalid-feedback">
                     Please provide more details regarding the event.
                   </div>
                 </div>
                 <div class="col-md-6">
                   <label for="eventCategory" class="form-label">Event Category</label>
-                  <select class="form-select" id="eventCategory" required>
+                  <select class="form-select" name="eventCategory" required>
                     <option value="">Not Specified</option>
                     <option>Sports</option>
                     <option>Politics</option>
@@ -135,16 +167,50 @@
                 </div>
                 <div class="col-md-6">
                   <label class="md-3">Claims</label>
-                  <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="reqJournalists">
-                  <label class="form-check-label" for="reqJournalists">Allow journalists to claim the event</label>
+                  <?php
+                    // the photographers and journalists are allowed to create events, but they automatically claim it upon creation
+                    // output the warning and the necessary buttons to allow the other role to claim the event
+                    if ($userSpeciality == "photographer") {
+                      echo '<p>You will claim the event as a photographer</p>';
+                      echo '
+                      <div class="form-check">
+                        <input type="checkbox" class="form-check-input" name="reqJournalists">
+                        <label class="form-check-label" for="reqJournalists">Allow journalists to claim the event</label>
+                      </div>
+                      ';
+                    }
+                    elseif ($userSpeciality == "journalist") {
+                      echo '<p>You will claim the event as a journalist</p>';
+                      echo '
+                      <div class="form-check">
+                        <input type="checkbox" class="form-check-input" name="reqPhotographers">
+                        <label class="form-check-label" for="reqPhotographers">Allow photographers to claim the event</label>
+                      </div>
+                      ';
+                    }
+                    else {
+                      echo '
+                      <div class="form-check">
+                        <input type="checkbox" class="form-check-input" name="reqJournalists">
+                        <label class="form-check-label" for="reqJournalists">Allow journalists to claim the event</label>
+                      </div>
+                      <div class="form-check">
+                        <input type="checkbox" class="form-check-input" name="reqPhotographers">
+                        <label class="form-check-label" for="reqPhotographers">Allow photographers to claim the event</label>
+                      </div>
+                      ';
+                    }
+                  ?>
                 </div>
-              <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="reqPhotographers">
-                <label class="form-check-label" for="reqPhotographers">Allow photographers to claim the event</label>
-              </div>
-              </div>
-              <button class="w-auto mt-3 btn btn-primary btn-lg" type="submit">Create Event</button>
+              <button class="w-auto mt-3 btn btn-primary btn-lg" type="submit" name="submit" value="submit">Create Event</button>
+              <?php
+              if (isset($_POST["submit"]) && count($errs)==0) {
+                echo '
+                <div class="alert alert-success">
+                  Event created successfully!
+                </div>';
+              }
+               ?>
               </div>
             </form>
           </div>
