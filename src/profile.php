403e9ea28f7db9cdefc2fce4a1a9882e325a089f
CadarIustin <iustin.cadar@student.nhlstenden.com>
2023-01-11 14:00:42 +0100

    I created a better Indentation and Spacing of the PHP page

--- src/profile.php
+++ src/profile.php
@@ -1,44 +1,44 @@
 <?php
-  session_start();
-  include_once "config.php";
+	session_start();
+	include_once "config.php";
   
   
-  if($_SERVER['REQUEST_METHOD']=='POST'){
-	  $test=$conn->prepare("SELECT password FROM Users WHERE email=:email");
-	  $test->bindParam(':email', $_SESSION['email']);   
-	  $verify=$test->execute();
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+		$test=$conn->prepare("SELECT password FROM Users WHERE email=:email");
+		$test->bindParam(':email', $_SESSION['email']);   
+		$verify=$test->execute();
 	  
-	  $curr_pass=filter_input(INPUT_POST, "CurrentPassword");
-	  if($curr_pass==$verify){
-			echo "yes";
-	  } else {echo"no";}
+		$curr_pass=filter_input(INPUT_POST, "CurrentPassword");
+		if($curr_pass==$verify){
+				echo "yes";
+		} 	else {echo"no";}
 		  
-  }
+	}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <?php include "head.php" ?>
-  <link href="styles/home.css" rel="stylesheet">
-  <title>Profile Page</title>
+	<?php include "head.php" ?>
+	<link href="styles/home.css" rel="stylesheet">
+	<title>Profile Page</title>
 </head>
 <body>
-  <main>
-    <div class="row m-0">
-      <div class="col-auto p-0">
-        <nav class="sidebar">
-          <?php include "navMenu.php" ?>
-        </nav>
-      </div>
+	<main>
+		<div class="row m-0">
+			<div class="col-auto p-0">
+				<nav class="sidebar">
+					<?php include "navMenu.php" ?>
+				</nav>
+			</div>
       <div class="col p-0">
-        <header class="py-4 shadow-sm">
-        </header>
-        <div class="p-4">
+			<header class="py-4 shadow-sm">
+			</header>
+		<div class="p-4">
           <div>
-            <?php
-            echo "<p><strong>Name: " . $_SESSION['name'] . "</strong></p>";
-            echo "<p><strong>Email: " . $_SESSION['email'] . "</strong></p>";
-            echo "<p><strong>Specialty: " . $_SESSION['speciality'] . "</strong></p>";
+				<?php
+				echo "<p><strong>Name: " . $_SESSION['name'] . "</strong></p>";
+				echo "<p><strong>Email: " . $_SESSION['email'] . "</strong></p>";
+				echo "<p><strong>Specialty: " . $_SESSION['speciality'] . "</strong></p>";
             
 			/*try {
               $sql = "SELECT COUNT(file_id) FROM Files WHERE user_id=:session_id";
@@ -53,35 +53,35 @@
             */
             
             
-            ?> 
+				?> 
 			
-			 </div>
-          <hr class="my-4">
-          <div>
-            <p><strong>Change Password</strong></p>
-			<form action="profile.php?yes" method="POST">
-				<div class="col-sm-6">
-				  <input type="text" name="currentPassword" class="form-control" id="CurrentPassword" placeholder="Current Password..." value="">
 				</div>
-				<p></p>
-				<div class="col-sm-6">
-				  <input type="text" name="newPassword" class="form-control" id="NewPassword" placeholder="New Password..." value="">
-				</div>
-				<p></p>
-				<div class="col-sm-6">
-				  <input type="text" name="confirmPassword" class="form-control" id="ConfirmPassword" placeholder="Confirm Password..." value="">
-				</div>
-				<p></p>
-				<button class="btn btn-primary" type="submit">Submit</button>
-			</form>
-          </div>
+			<hr class="my-4">
+			<div>
+				<p><strong>Change Password</strong></p>
+				<form action="profile.php?yes" method="POST">
+					<div class="col-sm-6">
+						<input type="text" name="currentPassword" class="form-control" id="CurrentPassword" placeholder="Current Password..." value="">
+					</div>
+						<p></p>
+					<div class="col-sm-6">
+						<input type="text" name="newPassword" class="form-control" id="NewPassword" placeholder="New Password..." value="">
+					</div>
+						<p></p>
+					<div class="col-sm-6">
+						<input type="text" name="confirmPassword" class="form-control" id="ConfirmPassword" placeholder="Confirm Password..." value="">
+					</div>
+						<p></p>
+					<button class="btn btn-primary" type="submit">Submit</button>
+				</form>
+			</div>
         </div>
         
-        <footer class="py-3 mt-5 d-flex justify-content-end shadow border-top navbar p-0">
-          <p class="mb-0 me-4">Copyright 2022 - Gemorskos. All rights reserved</p>
-        </footer>
-      </div>
-    </div>
-  </main>
+			<footer class="py-3 mt-5 d-flex justify-content-end shadow border-top navbar p-0">
+				<p class="mb-0 me-4">Copyright 2022 - Gemorskos. All rights reserved</p>
+			</footer>
+			</div>
+		</div>
+	</main>
 </body>
 </html> 
