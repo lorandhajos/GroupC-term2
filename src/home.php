d2f7873bcf990a20c712d3998d23712fba287453
Marijn Veenstra <marijn.veenstra1@student.nhlstenden.com>
2023-01-11 13:52:48 +0100

    fix spacing

--- src/home.php
+++ src/home.php
@@ -98,19 +98,19 @@
                       mkdir($target_dir);
                   }
                           
-                  // Check if file already exists
+                  //Check if file already exists
                   if (file_exists($target_file)) {
                     echo "Sorry, file already exists. ";
                     $uploadOk = 0;
                   }
 
-                  // // Check whether the file size is above 128mb
+                  //Check whether the file size is above 128mb
                   if ($_FILES["fileToUpload"]["size"] > 128000000) {
                     echo "Sorry, your file is too large, maximum filesize is 128mb. ";
                     $uploadOk = 0;
                   }
                   
-                  // Allow certain file formats
+                  //Allow certain file formats
                   if($FileType != "jpg" && $FileType != "png" && $FileType != "jpeg"
                   && $FileType != "gif" && $FileType != "docx" && $FileType != "txt" 
                   && $FileType != "odt" && $FileType != "dotx" && $FileType != "doc" 
@@ -119,10 +119,10 @@
                     $uploadOk = 0;
                   }
                 
-                  // Check if $uploadOk is set to 0 by an error
+                  //Check if $uploadOk is set to 0 by an error
                   if ($uploadOk == 0) {
                     echo "Sorry, your file was not uploaded. ";
-                  // if everything is ok, try to upload file
+                  //if everything is ok, try to upload file
                   } else {
                     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
                       echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
