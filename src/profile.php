f2bd1cce4bfe817dc9ac9a88265a68c353344ddb
CadarIustin <iustin.cadar@student.nhlstenden.com>
2023-01-11 17:03:38 +0100

    I changed my Indentation and i also changed the info of the user
    -I changed the way in each the emails, user names and speciality are displayed

--- src/profile.php
+++ src/profile.php
@@ -1,87 +1,78 @@
 <?php
-	session_start();
-	include_once "config.php";
-  
-  
-	if($_SERVER['REQUEST_METHOD']=='POST'){
-		$test=$conn->prepare("SELECT password FROM Users WHERE email=:email");
-		$test->bindParam(':email', $_SESSION['email']);   
-		$verify=$test->execute();
-	  
-		$curr_pass=filter_input(INPUT_POST, "CurrentPassword");
-		if($curr_pass==$verify){
-				echo "yes";
-		} 	else {echo"no";}
-		  
-	}
+  session_start();
+  include_once "config.php";
+
+  if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $test = $conn->prepare("SELECT password FROM Users WHERE email=:email");
+	$test->bindParam(':email', $_SESSION['email']);   
+	$test->execute();
+	
+    var_dump($verify);	
+	
+	$curr_pass = filter_input(INPUT_POST, "CurrentPassword");
+	if (password_verify($curr_pass, $verify)) {
+      echo "yes";
+	} else {
+      echo"no";
+	}  
+  }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<?php include "head.php" ?>
-	<link href="styles/home.css" rel="stylesheet">
-	<title>Profile Page</title>
-</head>
+  <head>
+    <?php include "head.php" ?>
+    <link href="styles/home.css" rel="stylesheet">
+    <title>Profile Page</title>
+  </head>
 <body>
-	<main>
-		<div class="row m-0">
-			<div class="col-auto p-0">
-				<nav class="sidebar">
-					<?php include "navMenu.php" ?>
-				</nav>
-			</div>
-      <div class="col p-0">
-			<header class="py-4 shadow-sm">
-			</header>
-		<div class="p-4">
-          <div>
-				<?php
-				echo "<p><strong>Name: " . $_SESSION['name'] . "</strong></p>";
-				echo "<p><strong>Email: " . $_SESSION['email'] . "</strong></p>";
-				echo "<p><strong>Specialty: " . $_SESSION['speciality'] . "</strong></p>";
-            
-			/*try {
-              $sql = "SELECT COUNT(file_id) FROM Files WHERE user_id=:session_id";
-              $stmt = $conn->prepare($sql);
-              $stmt->bindValue(':session_id', $_SESSION['user_id']);
-              $stmt->execute();
-            } catch (Exception $e) {
-              $error = "Failed to connect to the database";
-            }
-            $result = $stmt->fetch(PDO::FETCH_OBJ);
-            echo $result["COUNT(file_id)"];
-            */
-            
-            
-				?> 
-			
-				</div>
-			<hr class="my-4">
-			<div>
-				<p><strong>Change Password</strong></p>
-				<form action="profile.php?yes" method="POST">
-					<div class="col-sm-6">
-						<input type="text" name="currentPassword" class="form-control" id="CurrentPassword" placeholder="Current Password..." value="">
-					</div>
-						<p></p>
-					<div class="col-sm-6">
-						<input type="text" name="newPassword" class="form-control" id="NewPassword" placeholder="New Password..." value="">
-					</div>
-						<p></p>
-					<div class="col-sm-6">
-						<input type="text" name="confirmPassword" class="form-control" id="ConfirmPassword" placeholder="Confirm Password..." value="">
-					</div>
-						<p></p>
-					<button class="btn btn-primary" type="submit">Submit</button>
-				</form>
-			</div>
-        </div>
+  <main>
+	<div class="row m-0">
+	  <div class="col-auto p-0">
+		<nav class="sidebar">
+		<?php include "navMenu.php" ?>
+		</nav>
+	  </div>
+    <div class="col p-0">
+	  <header class="py-4 shadow-sm">
+   	  </header>
+  <div class="p-4">
+    <div>
+	  <?php
+		echo "<strong>Name:</strong>";
+	    echo "<input type='text' class='form-control' value='" . $_SESSION['name'] . "' disabled>";
+		echo "<strong>Email:</strong>";
+		echo "<input type='text' class='form-control' value='" . $_SESSION['email'] . "' disabled>";
+		echo "<strong>Specialty:</strong>";
+		echo "<input type='text' class='form-control' value='" . $_SESSION['speciality'] . "' disabled>";
+	  ?>
+	</div>
+  <hr class="my-4">
+	<div>
+	  <p><strong>Change Password</strong></p>
+	  <form action="profile.php" method="POST">
+	  <div class="col-sm-6">
+		<input type="text" name="currentPassword" class="form-control" id="CurrentPassword" placeholder="Current Password..." value="">
+	  </div>
+		<p></p>
+	  <div class="col-sm-6">
+		<input type="text" name="newPassword" class="form-control" id="NewPassword" placeholder="New Password..." value="">
+	  </div>
+		<p></p>
+	  <div class="col-sm-6">
+		<input type="text" name="confirmPassword" class="form-control" id="ConfirmPassword" placeholder="Confirm Password..." value="">
+	  </div>
+	    <p></p>
+	  <button class="btn btn-primary" type="submit">Submit</button>
+	  </form>
+	</div>
+  </div>
         
-			<footer class="py-3 mt-5 d-flex justify-content-end shadow border-top navbar p-0">
-				<p class="mb-0 me-4">Copyright 2022 - Gemorskos. All rights reserved</p>
-			</footer>
-			</div>
-		</div>
-	</main>
+	  <footer class="py-3 mt-5 d-flex justify-content-end shadow border-top navbar p-0">
+		<p class="mb-0 me-4">Copyright 2022 - Gemorskos. All rights reserved</p>
+	  </footer>
+	</div>
+	</div>
+  </main>
 </body>
 </html> 
