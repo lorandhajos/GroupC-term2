54bf903aa7e01b2c7386318595615b91691c0b1b
anastasialukanova <anastasia.lukanova@student.nhlstenden.com>
2023-01-13 12:11:16 +0100

    fixed indentation and added margin

--- src/profile.php
+++ src/profile.php
@@ -4,15 +4,15 @@
 
   if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $test = $conn->prepare("SELECT password FROM Users WHERE email=:email");
-	$test->bindParam(':email', $_SESSION['email']);
-	$test->execute();
+		$test->bindParam(':email', $_SESSION['email']);
+		$test->execute();
 	
 	if ($result = $test->fetch(PDO::FETCH_OBJ)) {
-      $curr_pass = filter_input(INPUT_POST, "currentPassword");
+    $curr_pass = filter_input(INPUT_POST, "currentPassword");
 	  $new_pass= filter_input(INPUT_POST, "newPassword");
 	  $confirm_pass= filter_input(INPUT_POST, "confirmPassword");
 	  if($new_pass==$confirm_pass){
-	   if (password_verify($curr_pass, $result->password)) {
+	  	if (password_verify($curr_pass, $result->password)) {
 		   $new_pass_crypt=password_hash($new_pass, PASSWORD_BCRYPT);
 		   $query_change=$conn->prepare("UPDATE Users SET password=:new_pass WHERE email=:user_email AND name=:user_name");
 		   $query_change->bindParam(':new_pass', $new_pass_crypt);
@@ -20,23 +20,23 @@
 		   $query_change->bindParam(':user_name', $_SESSION['name']);
 		   $query_change->execute();
 		   echo"Password changed successfully";
-	   } else {
-         echo "Error";
+	   	}else {
+        echo "Error";
 	    }
-	  } else {
-		  echo"<h5>Passwords not matching</h5>";
-	  }
-	}
+	  	}else {
+		  	echo"<h5>Passwords not matching</h5>";
+	  	}
+		}
   }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <?php include "head.php" ?>
-    <link href="styles/home.css" rel="stylesheet">
-    <title>Profile Page</title>
-  </head>
+<head>
+  <?php include "head.php" ?>
+  <link href="styles/home.css" rel="stylesheet">
+  <title>Profile Page</title>
+</head>
 <body>
   <main>
 	<div class="row m-0">
@@ -45,40 +45,41 @@
 		<?php include "navMenu.php" ?>
 		</nav>
 	  </div>
-      <div class="col p-0">
+    <div class="col p-0">
 	    <header class="py-4 shadow-sm"></header>
         <div class="p-4">
           <div><br>
 		  
 	        <?php
-		      echo "<h1 style='font-size: 1.3rem;'><strong>Name:</strong></h1>";
-	          echo "<input style='width:42.6vw;'  type='text' class='form-control' value='" . $_SESSION['name'] . "'  disabled>";
-              echo "<h1 style='font-size: 1.3rem;'><strong>Email:</strong></h1>";
-		      echo "<input style='width:42.6vw;' type='text' class='form-control' value='" . $_SESSION['email'] . "' disabled>";
-		      echo "<h1 style='font-size: 1.3rem;'><strong>Specialty:</strong></h1>";
-		      echo "<input style='width:42.6vw;' type='text' class='form-control' value='" . $_SESSION['speciality'] . "' disabled>";
+		      	echo "<h1 class='mt-1' style='font-size: 1.3rem;'><strong>Name:</strong></h1>";
+	          echo "<input style='width:42vw;'  type='text' class='form-control' value='" . $_SESSION['name'] . "'  disabled>";
+            echo "<h1 class='mt-1' style='font-size: 1.3rem;'><strong>Email:</strong></h1>";
+		      	echo "<input style='width:42vw;' type='text' class='form-control' value='" . $_SESSION['email'] . "' disabled>";
+		      	echo "<h1 class='mt-1' style='font-size: 1.3rem;'><strong>Specialty:</strong></h1>";
+		      	echo "<input style='width:42vw;' type='text' class='form-control' value='" . $_SESSION['speciality'] . "' disabled>";
 	        ?>
-	      </div><br>
+
+	      	</div><br>
           <hr class="my-4">
-	      <div>
-	        <p style="font-size: 1.3rem;"><strong>Change Password</strong></p><br>
-	          <form action="profile.php" method="POST">
-	            <div class="col-sm-6">
-                  <input type="text" name="currentPassword" class="form-control" id="CurrentPassword" placeholder="Current Password..." value="" style="box-shadow: inset 0 0 3px gray;"><br>
-	            </div>
-	            <div class="col-sm-6">
-		          <input type="text" name="newPassword" class="form-control" id="NewPassword" placeholder="New Password..." value="" style="box-shadow: inset 0 0 3px gray;"><br>
-	            </div>
-	            <div class="col-sm-6">
-		          <input type="text" name="confirmPassword" class="form-control" id="ConfirmPassword" placeholder="Confirm Password..." value="" style="box-shadow: inset 0 0 3px gray;"><br>
-	            </div>
-	            <button class="btn btn-primary" type="submit">Submit</button>
-	          </form>
-	      </div>
+					<div>
+						<p style="font-size: 1.3rem;"><strong>Change Password</strong></p><br>
+							<form action="profile.php" method="POST">
+								<div class="col-sm-6">
+									<input type="text" name="currentPassword" class="form-control" id="CurrentPassword" placeholder="Current Password..." value="" style="box-shadow: inset 0 0 3px gray;"><br>
+								</div>
+								<div class="col-sm-6">
+									<input type="text" name="newPassword" class="form-control" id="NewPassword" placeholder="New Password..." value="" style="box-shadow: inset 0 0 3px gray;"><br>
+								</div>
+								<div class="col-sm-6">
+									<input type="text" name="confirmPassword" class="form-control" id="ConfirmPassword" placeholder="Confirm Password..." value="" style="box-shadow: inset 0 0 3px gray;"><br>
+								</div>
+								<button class="btn btn-primary" type="submit">Submit</button>
+							</form>
+					</div>
         </div>
 	    <footer class="py-3 mt-5 d-flex justify-content-end shadow border-top navbar p-0">
-          <p class="mb-0 me-4">Copyright © 2022- Gemorskos. All rights reserved</p>
-        </footer>
+        <p class="mb-0 me-4">Copyright © 2022- Gemorskos. All rights reserved</p>
+      </footer>
 	  </div>
 	</div>
   </main>
