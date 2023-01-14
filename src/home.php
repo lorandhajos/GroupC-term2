5611611549d1ba32918e9ef817f35b8e5c05e2b1
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2023-01-14 19:03:01 +0100

    Made the home page work with the new database

--- src/home.php
+++ src/home.php
@@ -44,12 +44,13 @@
                   $description = $results["description"];
                   $creationDate = $results["creation_date"];
                   $eventDate = $results["event_date"];
+                  $eventCategory = $results["event_category"];
 
                   echo "
                   <div class='accordion-item'>
                     <h2 class='accordion-header' id='headingYourEvent$yourEventId'>
                       <button class='accordion-button collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#collapseYourEvent$yourEventId' aria-expanded='true' aria-controls='collapseYourEvent$yourEventId'>
-                      $eventName #$eventId
+                        #$eventId $eventName <span class='badge text-bg-info mx-1'>$eventCategory</span>
                       </button>
                     </h2>
                     <div id='collapseYourEvent$yourEventId' class='accordion-collapse collapse' aria-labelledby='headingYourEvent$yourEventId' data-bs-parent='#accordionExample1'>
@@ -75,7 +76,7 @@
                       </div>
                     </div>
                   </div>";
-                  $yourEventId ++;
+                  $yourEventId++;
                 } 
 
                 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -83,20 +84,21 @@
                   $target_dir = 'uploads/' . $eventId . '/';
                   $target_file = $target_dir . basename($_FILES['fileToUpload']['name']);
                   $error = '';
-                  $FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+                  $FileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-                  //connection to make an array with all the eventid's of the events that you have claimed
-                  $stmt = $conn->prepare("SELECT Claims.event_id FROM Claims INNER JOIN Users ON Claims.user_id = Users.user_id INNER JOIN Events ON Claims.event_id = Events.event_id WHERE Users.user_id = :id");
-                  $stmt->bindValue("id", $_SESSION["user_id"]);
+                  // Check if the user claimed the event
+                  $stmt = $conn->prepare("SELECT * FROM Events WHERE (Events.photographer_id = :userId OR Events.journalist_id = :userId) AND Events.event_id = :eventId");
+                  $stmt->bindValue("userId", $_SESSION["user_id"]);
+                  $stmt->bindValue("eventId", $eventId);
                   $stmt->execute();
-                  $test = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+                  $results = $stmt->fetch(PDO::FETCH_OBJ);
 
                   //check if a file was selected
                   if ($_FILES['fileToUpload']['size'] == 0 && empty($_FILES['fileToUpload']['name']) ){
                     $error = "No file was selected";
                   } else {
                     //check if you are assigned to the event, so whether or not the value hasnt been changed with f12
-                    if(!in_array($eventId, $test)) {
+                    if($stmt->rowCount() == 0) {
                       $error = "You don't have the rights to upload to this event";
                     }
 
@@ -113,28 +115,41 @@
 
                     //Check if file already exists
                     if (file_exists($target_file)) {
-                      $error = "Sorry, file already exists. ";
+                      $error = "Sorry, file already exists.";
                     }
 
                     //Check whether the file size is above 128mb
                     if ($_FILES["fileToUpload"]["size"] > 128000000) {
-                      $error = "Sorry, your file is too large, maximum filesize is 128mb. ";
+                      $error = "Sorry, your file is too large, maximum filesize is 128mb.";
                     }
 
                     //Allow certain file formats
                     if($FileType != "jpg" && $FileType != "png" && $FileType != "jpeg"
                     && $FileType != "docx" && $FileType != "txt" && $FileType != "odt") {
-                      $error = "Sorry, your files are not allowed. ";
+                      $error = "Sorry, your files are not allowed.";
                     }                    
                   }
 
-                  if (!empty($error)) {
-                    echo "$error";
-                  } else {  
-                    //if error is empty, try to upload file
+                  if (empty($error)) {
+                    // if error is empty, try to upload file
                     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                      echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+                      try {
+                        $sql = "INSERT INTO Files(user_id, event_id, upload_date, file_name) VALUES (:user_id, :event_id, :upload_date, :file_name)";
+                        $stmt = $conn->prepare($sql);
+                        $stmt->bindValue(':user_id', $_SESSION["user_id"]);
+                        $stmt->bindValue(':event_id', $results->event_id);
+                        $stmt->bindValue(':upload_date', date("y-m-d"));
+                        $stmt->bindValue(':file_name', basename($_FILES['fileToUpload']['name']));
+
+                        if ($stmt->execute()) {
+                          echo "The file ". htmlspecialchars(basename($_FILES["fileToUpload"]["name"])). " has been uploaded.";
+                        }
+                      } catch (PDOException $e) {
+                        echo $e->getMessage();
+                      }
                     }
+                  } else {  
+                    echo $error;
                   }
                 }
                 echo "</div>";
@@ -143,15 +158,28 @@
             <h2 class="mt-4 mb-4">Unclaimed Events</h2>
             <div class="accordion" id="accordionExample2">
               <?php
-                $stmt = $conn->prepare("SELECT * FROM Events WHERE Events.journalist_id IS NULL AND Events.photographer_id IS NULL");
+                // Get all events that can be claimed by the user and not claimed yet
+                if ($_SESSION["speciality"] == "photographer") {
+                  $sql = "SELECT * FROM Events WHERE (Events.claim_type = 1 AND Events.photographer_id IS NULL) OR
+                  (Events.claim_type = 3 AND Events.photographer_id IS NULL)";
+                } else if ($_SESSION["speciality"] == "journalist") {
+                  $sql = "SELECT * FROM Events WHERE (Events.claim_type = 2 AND Events.journalist_id IS NULL) OR
+                  (Events.claim_type = 3 AND Events.journalist_id IS NULL)";
+                } else if ($_SESSION["speciality"] == "editor") {
+                  $sql = "SELECT * FROM Events WHERE (Events.claim_type = 1 AND Events.photographer_id IS NULL) OR
+                  (Events.claim_type = 2 AND Events.journalist_id IS NULL) OR
+                  (Events.claim_type = 3 AND (Events.journalist_id IS NULL OR Events.photographer_id IS NULL))";
+                }
+
+                $stmt = $conn->prepare($sql);
                 $stmt->bindColumn("event_id", $eventId);
                 $stmt->bindColumn("name", $eventName);
                 $stmt->bindColumn("description", $description);
                 $stmt->bindColumn("event_date", $eventDate);
                 $stmt->bindColumn("creation_date", $creationDate);
-                
+                $stmt->bindColumn("event_category", $eventCategory);
+                $stmt->bindColumn("claim_type", $claimType);
                 $stmt->execute();
-                $results = $stmt->execute();
 
                 $accordionId = 0;
                 while ($results = $stmt->fetch()) {
@@ -159,7 +187,7 @@
                   <div class='accordion-item'>
                     <h2 class='accordion-header' id='heading$accordionId'>
                       <button class='accordion-button collapsed d-flex' type='button' data-bs-toggle='collapse' data-bs-target='#collapse$accordionId' aria-expanded='true' aria-controls='collapse$accordionId'>
-                      $eventName #$eventId
+                        #$eventId $eventName <span class='badge text-bg-info mx-1'>$eventCategory</span>
                       </button>
                     </h2>
                     <div id='collapse$accordionId' class='accordion-collapse collapse' aria-labelledby='heading$accordionId' data-bs-parent='#accordionExample2'>
@@ -186,22 +214,36 @@
                     </div>
                   </div>
                   ";
-                  $accordionId ++;
+                  $accordionId++;
                 }
               ?>
             </div> 
             <h2 class="mt-4 mb-4">Claimed Events</h2>
             <div class="accordion" id="accordionExample3">
               <?php
-                $stmt = $conn->prepare("SELECT * FROM Events WHERE Events.journalist_id IS NOT NULL OR Events.photographer_id IS NOT NULL");
+                // Get all events that are claimed but not by us
+                if ($_SESSION["speciality"] == "photographer") {
+                  $sql = "SELECT * FROM Events WHERE (Events.claim_type = 1 AND Events.photographer_id IS NOT NULL AND Events.photographer_id != :user_id) OR
+                  (Events.claim_type = 3 AND Events.photographer_id IS NOT NULL AND Events.photographer_id != :user_id)";
+                } else if ($_SESSION["speciality"] == "journalist") {
+                  $sql = "SELECT * FROM Events WHERE (Events.claim_type = 2 AND Events.journalist_id IS NOT NULL AND Events.journalist_id != :user_id) OR
+                  (Events.claim_type = 3 AND Events.journalist_id IS NOT NULL AND Events.journalist_id != :user_id)";
+                } else if ($_SESSION["speciality"] == "editor") {
+                  $sql = "SELECT * FROM Events WHERE (Events.claim_type = 1 AND Events.photographer_id IS NOT NULL) OR
+                  (Events.claim_type = 2 AND Events.journalist_id IS NOT NULL) OR
+                  (Events.claim_type = 3 AND (Events.journalist_id IS NOT NULL AND Events.photographer_id IS NOT NULL))
+                  AND (Events.photographer_id != :user_id AND Events.journalist_id != :user_id)";
+                }
+
+                $stmt = $conn->prepare($sql);
+                $stmt->bindValue(':user_id', $_SESSION["user_id"]);
                 $stmt->bindColumn("event_id", $eventId);
                 $stmt->bindColumn("name", $eventName);
                 $stmt->bindColumn("description", $description);
                 $stmt->bindColumn("event_date", $eventDate);
                 $stmt->bindColumn("creation_date", $creationDate);
-                
+                $stmt->bindColumn("event_category", $eventCategory);
                 $stmt->execute();
-                $results = $stmt->execute();
 
                 $claimedId = 0;
                 while ($results = $stmt->fetch()) {
@@ -209,7 +251,7 @@
                   <div class='accordion-item'>
                     <h2 class='accordion-header' id='headingClaimed$claimedId'>
                       <button class='accordion-button collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#collapseClaimed$claimedId' aria-expanded='true' aria-controls='collapseClaimed$claimedId'>
-                      $eventName #$eventId
+                        #$eventId $eventName <span class='badge text-bg-info mx-1'>$eventCategory</span>
                       </button>
                     </h2>
                     <div id='collapseClaimed$claimedId' class='accordion-collapse collapse' aria-labelledby='headingClaimed$claimedId' data-bs-parent='#accordionExample3'>
@@ -229,7 +271,7 @@
                     </div>
                   </div>
                   ";
-                  $claimedId ++;
+                  $claimedId++;
                 }
               ?>
             </div> 
