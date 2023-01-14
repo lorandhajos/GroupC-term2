010a88680d8c08686192e2c502b3506dcf0f9f26
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2023-01-14 15:24:11 +0100

    added claimType enum

--- src/pages/config.php
+++ src/pages/config.php
@@ -4,6 +4,12 @@
   $pass = $_ENV["DB_PASSWORD"];
   $db = $_ENV["DB_NAME"];
 
+  enum claimType: int {
+    case PHOTOGRAPHER = 1;
+    case JOURNALIST = 2;
+    case ALL = 3;
+  }
+
   try {
     $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
