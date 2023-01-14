8f7a375e906d4ebf5d36927883f2ff115a51c09c
Andrii Chumakov <andrii.chumakov@student.nhlstenden.com>
2023-01-14 18:48:39 +0100

    claimEvents.php works with the current database setup

--- src/claimEvents.php
+++ src/claimEvents.php
@@ -11,10 +11,16 @@
   // check whether the form has been submitted
   if (isset($_POST["submit"])) {
     try {
+      $targetColumn = "";
+      if ($_SESSION["speciality"] === "journalist") {
+        $targetColumn = "journalist_id";
+      } elseif ($_SESSION["speciality"] === "photographer") {
+        $targetColumn = "photographer_id";
+      }
       // preparing the sql query
       // put the value of the superglobal $_SESSION["user_id"] into the user_id that will be connected to the event
       // and then again but for the event_id
-      $sql = "INSERT INTO Claims(user_id, event_id) VALUES (:user_id, :event_id)";
+      $sql = "UPDATE Events SET $targetColumn = :user_id WHERE event_id = :event_id;";
       $stmt = $conn->prepare($sql);
       $stmt->bindValue(':user_id', $_SESSION["user_id"]);
       $stmt->bindValue(':event_id', $_POST["event_id"]);
